c272559148cd63c0dd40a6a780a49696aed55e0d
Sreng Shuan <31960170+Sreng-S@users.noreply.github.com>
2020-07-06 21:06:57 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -30,8 +30,8 @@ const router = new Router({
       route('/crud/:resource/create', 'CrudForm', 'create'),
       route('/crud/:resource/:id/:action', 'CrudForm', 'action'),
       route('/crud/:resource/:action', 'CrudForm', 'indexAction'),
-      route('/example', 'Example'),
-      route('/settings', 'Settings'),
+      // route('/example', 'Example'),
+      // route('/settings', 'Settings'),
       route('/theme', 'Theme'),
       route('/chat', 'Chat'),
       route('/about', 'About')
